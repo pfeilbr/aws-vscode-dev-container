63c2e4236f1504a10427d945d520f428730bc123
Brian Pfeil <brian.pfeil@gmail.com>
2022-07-24 16:04:09 -0400

    added pyenv setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,3 +9,13 @@ brew install \
     pyenv \
     tree \
     terraform
+
+# pyenv setup
+pyenv install 3.9.13
+pyenv global 3.9.13
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+source ~/.bashrc
