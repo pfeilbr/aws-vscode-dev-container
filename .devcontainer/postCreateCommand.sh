0ed3f7abb842daa2a62cb9e3987307056b85fe8d
Brian Pfeil <brian.pfeil@gmail.com>
2022-08-17 16:16:35 -0400

    IJavascript is a javascript kernel for the Jupyter notebook

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -20,4 +20,8 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
+# install IJavascript is a javascript kernel for the Jupyter notebook
+npm install -g ijavascript
+ijsinstall
+
 source ~/.bashrc
